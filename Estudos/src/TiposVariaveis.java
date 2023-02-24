8424772cc2a6a7bcd4293b96de485d9fa677179b
Guilherme Albano <guilhermenovolg@gmail.com>
2023-02-24 10:32:33 -0300

    Operador ternário

--- Estudos/src/TiposVariaveis.java
+++ Estudos/src/TiposVariaveis.java
@@ -2,15 +2,14 @@
 public class TiposVariaveis {
 		public static void main (String[]args) {
 			System.out.println("Ola, estou usando o eclipse");
-			//decremento
-			//para incrementar ou decrementar, precisamos atribuir a operação, como a seguir:
-			int numero = 5;
+			int a,b;
+			a = 7;
+			b = 5;
+			String resultado = a < b ? "Verdade ":"Falso";
+			System.out.println(resultado);
 			
-			System.out.println(++ numero);
-
 			
-			numero = --numero;
-			System.out.println(numero);
+			//operador ternário:
 			
 		}
 		
