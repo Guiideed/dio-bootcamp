b2f9bad88381bc7e7ff237f6eb583e55e94ce93b
Guilherme Albano <guilhermenovolg@gmail.com>
2023-02-24 10:25:20 -0300

    Incrementação e decrementacão

--- Estudos/src/TiposVariaveis.java
+++ Estudos/src/TiposVariaveis.java
@@ -1,28 +1,16 @@
 
 public class TiposVariaveis {
 		public static void main (String[]args) {
-			System.out.println("Olá, estou usando o eclipse");
-			byte idade = 123;
-			short ano = 2023;
-			int cep = 7270000; //se atentar pois, se o cep começar com zero, seja melhor usar outro tipo
-			long cpf = 2669600019L; //se atentar pois, pode ocorrer o mesmo caso de do cep
-			float pi = 3.14F;
+			System.out.println("Ola, estou usando o eclipse");
+			//decremento
+			//para incrementar ou decrementar, precisamos atribuir a operação, como a seguir:
+			int numero = 5;
 			
+			System.out.println(++ numero);
+
 			
-			/*constante
-			final int VALOR_FIXO = 5;
-			int VALOR_FIXO = 2;  causa o erro pois não pode ser alterado*/
-			
-			//sobre operadores
-			int soma = 1+1;
-			int subtracao = 1-1;
-			int multiplicacao = 2*2;
-			int divisao = 4 / 2;
-			int modulo = 18%3;
-			double resultado = (10+2)*(11-10);
-			
-			System.out.println(soma + " "+subtracao+" "+modulo+" ");
-			
+			numero = --numero;
+			System.out.println(numero);
 			
 		}
 		
