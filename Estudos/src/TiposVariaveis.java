c1360e7965df4089ff1d871eaf2c99f254ca699a
Guilherme Albano <guilhermenovolg@gmail.com>
2023-02-24 09:41:27 -0300

    Constantes

--- Estudos/src/TiposVariaveis.java
+++ Estudos/src/TiposVariaveis.java
@@ -7,5 +7,10 @@ public class TiposVariaveis {
 			int cep = 7270000; //se atentar pois, se o cep começar com zero, seja melhor usar outro tipo
 			long cpf = 2669600019L; //se atentar pois, pode ocorrer o mesmo caso de do cep
 			float pi = 3.14F;
+			
+			
+			/*constante
+			final int VALOR_FIXO = 5;
+			int VALOR_FIXO = 2;  causa o erro pois não pode ser alterado*/
 		}
 }
