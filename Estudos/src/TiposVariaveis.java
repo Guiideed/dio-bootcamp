716e47fd685e61f391d5c3b4f9b43bb755a34b9e
Guilherme Albano <guilhermenovolg@gmail.com>
2023-02-24 09:36:50 -0300

    Entendendo mais a fundo variáveis

--- Estudos/src/TiposVariaveis.java
+++ Estudos/src/TiposVariaveis.java
@@ -2,5 +2,10 @@
 public class TiposVariaveis {
 		public static void main (String[]args) {
 			System.out.println("Olá, estou usando o eclipse");
+			byte idade = 123;
+			short ano = 2023;
+			int cep = 7270000; //se atentar pois, se o cep começar com zero, seja melhor usar outro tipo
+			long cpf = 2669600019L; //se atentar pois, pode ocorrer o mesmo caso de do cep
+			float pi = 3.14F;
 		}
 }
